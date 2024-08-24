89fe864dd65ecf25c7418ffcfa3e5901a4e3c0d0
Fatimazulfiqar1 <fzulfiqar648@gmail.com>
2024-08-23 23:20:29 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+// Script File
+
+// Home Section Starts
+var menuBtn = document.querySelector('.main-navbar .menu-btn');
+var menuList = document.querySelector('.main-navbar .nav-list');
+var menuListItems = document.querySelectorAll('.nav-list li a');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menuList.classList.toggle('active');
+});
+
+for(var i = 0; i < menuListItems.length; i++){
+	menuListItems[i].addEventListener('click', menuItemClicked);
+}
+function menuItemClicked(){
+	menuBtn.classList.remove('active');
+	menuList.classList.remove('active');
+}
+
+var homeSection = document.querySelector('.home');
+window.addEventListener('scroll', pageScrollFunction);
+window.addEventListener('load', pageScrollFunction);
+
+function pageScrollFunction(){
+	if(window.scrollY > 120){
+		homeSection.classList.add('active');
+	}
+	else{
+		homeSection.classList.remove('active');
+	}
+}
+// Home Section Ends
+
+// Partners Section Starts 
+$('.partners-slider').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    margin:10,
+    nav:true,
+    navText:["<i class='fa-solid fa-arrow-left'></i>",
+             "<i class='fa-solid fa-arrow-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        500:{
+            items:2
+        },
+        700:{
+            items:3
+        },
+        1000:{
+        	items:5
+        }
+    }
+})
+// Partners Section Ends 
+
+// Testimonials Section Starts
+$('.testimonials-slider').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    margin:10,
+    nav:true,
+    navText:["<i class='fa-solid fa-arrow-left'></i>",
+             "<i class='fa-solid fa-arrow-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        }
+    }
+})
+// Testimonials Section Ends
+
+// login projact
+function login() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var message = document.getElementById('loginMessage');
+  
+    // Basic validation (you should implement proper validation and authentication)
+    if (username === 'admin' && password === 'password') {
+      message.innerHTML = 'Login successful!';
+      // Redirect or do something after successful login
+    } else {
+      message.innerHTML = 'Invalid username or password.';
+    }
+  }
+  
